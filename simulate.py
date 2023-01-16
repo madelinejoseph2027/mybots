f1f0c2c3ccb35524390c19ea660a53479df2d701
Maddie Joseph <maddiejoseph@Maddies-Air-5.attlocal.net>
2023-01-15 20:42:24 -0600

    Updating simulate.py for Sensors

--- simulate.py
+++ simulate.py
@@ -1,9 +1,11 @@
+#Imports
 import pybullet as p
 import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
 
+#Set up world
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -12,17 +14,31 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+#Launch world
 pyrosim.Prepare_To_Simulate(robotId)
 
+#Simulate robots
 iterations = 1000
+
 backLegSensorValues = np.zeros(iterations)
+frontLegSensorValues = np.zeros(iterations)
 
 for i in range(iterations):
     p.stepSimulation()
+    
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+    
     time.sleep(1/60)
     #print(i)
 
+#Save sensor values
+np.save("data/backLegSensorValues", backLegSensorValues)
+np.save("data/frontLegSensorValues", frontLegSensorValues)
+
+#Shut down GUI
 p.disconnect()
 
-print(backLegSensorValues)
+#print(backLegSensorValues)
+#print(frontLegSensorValues)
+

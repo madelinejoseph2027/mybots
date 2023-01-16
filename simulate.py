fbd3472cb8da0b764e3881768a267e9171115c0d
Maddie Joseph <maddiejoseph@Maddies-Air-5.attlocal.net>
2023-01-15 23:24:53 -0600

    Updating simulate for Sensors

--- simulate.py
+++ simulate.py
@@ -4,6 +4,8 @@ import time
 import pybullet_data
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import random
+
 
 #Set up world
 physicsClient = p.connect(p.GUI)
@@ -14,31 +16,68 @@ planeId = p.loadURDF("plane.urdf")
 robotId = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 
+
 #Launch world
 pyrosim.Prepare_To_Simulate(robotId)
 
-#Simulate robots
+
+#Variables for simulating robots
 iterations = 1000
+
+backLegAmplitude = np.pi/4.0
+backLegFrequency = 6
+backLegPhaseOffset = 0
+frontLegAmplitude = np.pi/4.0
+frontLegFrequency = 6
+frontLegPhaseOffset = np.pi/3.0
+
+angleRange = np.linspace(0, 2*np.pi, iterations+1)
+backTargetAngles = backLegAmplitude*np.sin(backLegFrequency*angleRange+backLegPhaseOffset)
+frontTargetAngles = frontLegAmplitude*np.sin(frontLegFrequency*angleRange+frontLegPhaseOffset)
+
+maxForce = 100
 
 backLegSensorValues = np.zeros(iterations)
 frontLegSensorValues = np.zeros(iterations)
 
+
+#Simulate robots
 for i in range(iterations):
+    #Step simulation
     p.stepSimulation()
     
+    #Track sensor values in each link
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     
-    time.sleep(1/60)
-    #print(i)
+    #Put motors in each joint
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = 'Torso_BackLeg',
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = backTargetAngles[i],
+        maxForce = maxForce)
+    
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robotId,
+        jointName = 'Torso_FrontLeg',
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = frontTargetAngles[i],
+        maxForce = maxForce)
+    
+    #Slow down the stepping so we can see things happening the world
+    time.sleep(1/240)
+
 
 #Save sensor values
 np.save("data/backLegSensorValues", backLegSensorValues)
 np.save("data/frontLegSensorValues", frontLegSensorValues)
 
+
 #Shut down GUI
 p.disconnect()
 
+
+
 #print(backLegSensorValues)
 #print(frontLegSensorValues)
-

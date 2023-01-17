04fdb77b5ce5ac63469779400c51dc1fb5f20631
Maddie Joseph <maddiejoseph@Maddies-Air-5.attlocal.net>
2023-01-16 20:51:38 -0600

    Update

--- constants.py
+++ constants.py
@@ -9,8 +9,8 @@ backLegFrequency = 6
 backLegPhaseOffset = 0
 frontLegAmplitude = np.pi/4.0
 frontLegFrequency = 6
-frontLegPhaseOffset = np.pi/3.0
+frontLegPhaseOffset = 0
 
 maxForce = 100
 
-sleep = 1/240
+sleep = 1/120

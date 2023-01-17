235d07335338755769662c1bd75d96d7f9e2c4b8
Maddie Joseph <maddiejoseph@Maddies-Air-5.attlocal.net>
2023-01-16 20:18:54 -0600

    Update

---
+++ constants.py
@@ -0,0 +1,16 @@
+#Imports
+import numpy as np
+
+#Variables
+iterations = 1000
+
+backLegAmplitude = np.pi/4.0
+backLegFrequency = 6
+backLegPhaseOffset = 0
+frontLegAmplitude = np.pi/4.0
+frontLegFrequency = 6
+frontLegPhaseOffset = np.pi/3.0
+
+maxForce = 100
+
+sleep = 1/240

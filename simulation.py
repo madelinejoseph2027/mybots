164b8541e14962a9c4e5c0cecc4e530af445c824
Maddie Joseph <maddiejoseph@Maddies-Air-5.attlocal.net>
2023-01-16 20:16:41 -0600

    Update

--- simulation.py
+++ simulation.py
@@ -8,22 +8,24 @@ import pyrosim.pyrosim as pyrosim
 import constants as c
 from world import WORLD
 from robot import ROBOT
+from sensor import SENSOR
+from motor import MOTOR
 
 class SIMULATION:
     def __init__(self):
-            #Set up world
-            self.physicsClient = p.connect(p.GUI)
-            p.setAdditionalSearchPath(pybullet_data.getDataPath())
-
-            p.setGravity(0,0,-9.8)
-            
             self.world = WORLD()
             self.robot = ROBOT()
 
     def Run(self):
         for i in range(c.iterations):
             p.stepSimulation()
+            
+            self.robot.Sense(i)
+            self.robot.Act(i)
+            
             time.sleep(c.sleep)
                         
     def __del__(self):
+        #SENSOR.Save_Values()
+        #MOTOR.Save_Values()
         p.disconnect()

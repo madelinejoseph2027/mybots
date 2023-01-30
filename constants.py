a0d6390f45e1021457cce0ef49c5e58f6b896f81
Maddie Joseph <maddiejoseph@Maddies-Air-5.attlocal.net>
2023-01-30 02:07:07 -0600

    Update

--- constants.py
+++ constants.py
@@ -4,6 +4,15 @@ import numpy as np
 #Variables
 iterations = 1000
 
+maxForce = 100
+
+sleep = 1/120
+
+
+numSensorNeurons = 4
+numMotorNeurons = 8
+
+motorJointRange = 1.0
 backLegAmplitude = np.pi/4.0
 backLegFrequency = 6
 backLegPhaseOffset = 0
@@ -11,9 +20,6 @@ frontLegAmplitude = np.pi/4.0
 frontLegFrequency = 6
 frontLegPhaseOffset = 0
 
-maxForce = 100
-
-sleep = 1/120
 
 numberOfGenerations = 10
 populationSize = 10
